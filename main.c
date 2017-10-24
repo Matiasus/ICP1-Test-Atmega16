7200e2e50be4359c04e947064cf35d7bc6b60237
Marián <Matiasus@users.noreply.github.com>
2017-10-24 16:03:00 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,53 @@
 #include <avr/interrupt.h>
 #include <avr/io.h>
 
+// gathred times
+volatile uint16_t times[3];
+// need 3 times for determining duty cycle
+//    ______          ______
+// __|@@@@@@|________|@@@@@@|_______
+//   |      |        |
+//   t1     t2      t3
+volatile uint8_t cycles = 0;
+// start with falling edge
+volatile uint8_t falling = 1;
+
+/**
+ * @description Input capture interrupt routine
+ *
+ * @param  TIMER1_CAPT_vect
+ * @return Void
+ */
+ISR(TIMER1_CAPT_vect)
+{
+  if (cycles < 2) {
+    // detect rising edge
+    if (falling == 0) {
+      // write low byte
+      times[cycles] = (0x00ff & ICR1L);
+      // write high byte
+      times[cycles] |= (ICR1H << 8);
+      // falling edge trigger
+      TCCR1B &= ~(1 << ICES1);     
+      // detect falling edge
+      falling = 1;
+      // decrement cycle
+      cycles++;
+    } else {
+      // write low byte
+      values[cycles] = (0x00ff & ICR1L);
+      // write high byte
+      values[cycles] |= (ICR1H << 8);
+      // falling edge trigger
+      TCCR1B |= (1 << ICES1);   
+      // detect falling edge
+      falling = 1;
+      // decrement cycle
+      cycles++;      
+    }
+  }
+}
+
 /**
  * @description Main function
  *
@@ -9,17 +56,6 @@
  */
 int main (void)
 {
-  // need 3 times for determining duty cycle
-  //    ______          ______
-  // __|@@@@@@|________|@@@@@@|_______
-  //   |      |        |
-  //   t1     t2      t3
-  uint8_t cycles = 3;
-  // start with falling edge
-  uint8_t falling = 1;
-  // storage values
-  uint16_t values[3];
-  
   // COM1A1, COM1A0 = 0; OC1A disconnected
   // COM1B1, COM1B0 = 0; OC1B disconnected
   //  FOC1A,  FOC1B = 0; 
@@ -34,42 +70,6 @@ int main (void)
   TIFR = (1 << ICF1);
   // input capture interrupt routine enable
   TIMSK = (1 << TICIE1);
-  // 3 cycles loop to grab duty cycle
-  while (cycles) {
-    // check if input capture flag is set
-    if (TIFR & (1 << ICF1)) {
-      // detect rising edge
-      if (falling == 0) {
-        // write low byte
-        values[cycles-1] = (0x00ff & ICR1L);
-        // write high byte
-        values[cycles-1] |= (ICR1H << 8);
-        // falling edge trigger
-        TCCR1B &= ~(1 << ICES1);
-        // null input capture flag
-        TIFR = (1 << ICF1);        
-        // detect falling edge
-        falling = 1;
-        // decrement cycle
-        cycles--;
-      } else {
-        // write low byte
-        values[cycles-1] = (0x00ff & ICR1L);
-        // write high byte
-        values[cycles-1] |= (ICR1H << 8);
-        // falling edge trigger
-        TCCR1B |= (1 << ICES1);
-        // null input capture flag
-        TIFR = (1 << ICF1);        
-        // detect falling edge
-        falling = 1;
-        // decrement cycle
-        cycles--;      
-      }
-    }
-  }
-  // calc duty cycle
-  uint16_t duty = (values[1]-values[0])/(values[2]-values[0])
   
   // return value
   return 0;
